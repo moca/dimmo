2bef3dc68eab8b478061e624b8baa885fbf98bba
Arturo Mondragón <moca.art@gmail.com>
2020-12-22 16:39:22 +0100

    Add gitignore

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets) 
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
@@ -15,18 +21,31 @@ df = pd.DataFrame({
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(
-  children=[
-    html.H1(children="Hello Maestro"),
-    html.Div(children="""
-      Dash: A web application framework for python.
-    """),
-    dcc.Graph(
-      id = 'example-graph',
-      figure=fig
-    )
-  ]
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
 )
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Dash: A web application framework for Python.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    )
+])
 
 if __name__  == "__main__":
   app.run_server(debug=True) 
